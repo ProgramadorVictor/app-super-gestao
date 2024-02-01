1649eaf585696663817765788d0a6b944678238f
ProgramadorVictor <victorochaandrade@gmail.com>
2024-02-01 14:12:43 -0300

    att

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -34,68 +34,63 @@ class FornecedorController extends Controller
         return view('app.fornecedor.listar', ['fornecedores' => $fornecedores, 'req' => $req->all()]);
         // return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
     }
-    public function adc(Request $req){
-        dd("toma no cu");
-        return redirect()->route('app.home');
+    public function adicionar(Request $req)
+    {
+        $msg = '';
+        $classe = '';
+        $dados = [
+            'mensagem' => $msg,
+            'classe' => $classe,
+        ]; //Padrão utilizado pela prefeitura na criação de sistemas.
+        //AQUI É O METODO DE ADIÇÃO
+        if ($req->input('_token') != '' && $req->id == '') {
+            $regras = [
+                'nome' => 'required|min:3max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+            $feedback = [
+                //:attribute é uma instrução que recupera o nome do campo.
+                //No caso se nome, não for preenchido retornar o campo nome.
+                'required' => 'O campo é :attribute é requerido',
+                'nome.min' => 'O campo deve ter no minimo 3 caracteres',
+                'nome.max' => 'O campo deve ter no maximo 40 caracteres',
+                'uf.min' => 'O campo deve ter no minimo 2 caracteres',
+                'uf.max' => 'O campo deve ter no maximo 2 caracteres',
+                'email.email' => 'O campo email não foi preenchido corretamente'
+            ];
+            $req->validate($regras, $feedback);
+            //Fazendo o validate passando as regras e o feedback.
+            $fornecedor = new Fornecedor(); //Objeto fornecedor trazendo ele. //Instanciando um objeto
+            $fornecedor->create($req->all()); //OBS: Muito interessante essa ideia, vou usar em outros projetos.
+            //$fornecedor->create($req->all()); //É interessante pois está preenchedo o fornecedor com a permissao da variavel fillable 'protected $fillable = ['nome','site','uf','email'];'
+            //A variavel fillable é definida dentro da model de Fornecedor.
+            $msg = 'Cadastro realizado com sucesso';
+            $dados = [
+                'mensagem' => $msg,
+                // 'classe' => //Costume utilizado na criação de sites para a prefeitura a classe era passado como o alert do bootstrap
+                // 'alert-success shows', entre outros alertas.
+            ];
+            // echo 'Cadastrado com sucesso';
+        }
+        //AQUI É O MÉTODO DE EDIÇÃO, VERIFICAMOS SE O ID É DIFERENTE DE NULO E O INPUT É DIFERENTE DE VAZIO
+        if ($req->input('_token') != '' && $req->id != '') {
+            $fornecedor = Fornecedor::find($req->id); //BUSCANDO PELO ID
+            $update = $fornecedor->update($req->all());
+            //Aqui é a saida se deu certo o registro é alterado com sucesso, se não o registro não é alterado.
+            if($update){ //Se ocorre o update, faz as mensagens de acordo para a views.
+                $msg = "Update realizado com sucesso";
+            }else{
+                $msg = "Update apresentou problemas";
+            }
+            return redirect()->route('app.fornecedor.editar', ['id'=> $fornecedor->id, 'msg' => $msg, ]);
+        }
+        // print_r($req->all()); //Mostrando os dados do formulario com o _token
+        //OBS: NESSE RETORNO FOI UMA MODIFICAÇÃO FEITA POR EU, PARA PASSA A VARIAVEL FORNECEDOR PARA LA, PÓS EDIÇÃO EU QUERO QUE OS DADOS PERSITEM
+        return view('app.fornecedor.adicionar', ['msg' => $msg]); //Temos um problema que pode causar sobrecarga de dados, ao atualiza a pagina toda hora o request é mandado
+        //Aqui ocorre uma sobrecarga do banco de dados se você redirecionar para a view, ou seja. Temos um problema aqui.
     }
-
-    // public function adicionar(Request $req)
-    // {
-    //     $msg = '';
-    //     $classe = '';
-    //     $dados = [
-    //         'mensagem' => $msg,
-    //         'classe' => $classe,
-    //     ]; //Padrão utilizado pela prefeitura na criação de sistemas.
-    //     //AQUI É O METODO DE ADIÇÃO
-    //     if ($req->input('_token') != '' && $req->id == '') {
-    //         $regras = [
-    //             'nome' => 'required|min:3max:40',
-    //             'site' => 'required',
-    //             'uf' => 'required|min:2|max:2',
-    //             'email' => 'email'
-    //         ];
-    //         $feedback = [
-    //             //:attribute é uma instrução que recupera o nome do campo.
-    //             //No caso se nome, não for preenchido retornar o campo nome.
-    //             'required' => 'O campo é :attribute é requerido',
-    //             'nome.min' => 'O campo deve ter no minimo 3 caracteres',
-    //             'nome.max' => 'O campo deve ter no maximo 40 caracteres',
-    //             'uf.min' => 'O campo deve ter no minimo 2 caracteres',
-    //             'uf.max' => 'O campo deve ter no maximo 2 caracteres',
-    //             'email.email' => 'O campo email não foi preenchido corretamente'
-    //         ];
-    //         $req->validate($regras, $feedback);
-    //         //Fazendo o validate passando as regras e o feedback.
-    //         $fornecedor = new Fornecedor(); //Objeto fornecedor trazendo ele. //Instanciando um objeto
-    //         $fornecedor->create($req->all()); //OBS: Muito interessante essa ideia, vou usar em outros projetos.
-    //         //$fornecedor->create($req->all()); //É interessante pois está preenchedo o fornecedor com a permissao da variavel fillable 'protected $fillable = ['nome','site','uf','email'];'
-    //         //A variavel fillable é definida dentro da model de Fornecedor.
-    //         $msg = 'Cadastro realizado com sucesso';
-    //         $dados = [
-    //             'mensagem' => $msg,
-    //             // 'classe' => //Costume utilizado na criação de sites para a prefeitura a classe era passado como o alert do bootstrap
-    //             // 'alert-success shows', entre outros alertas.
-    //         ];
-    //         // echo 'Cadastrado com sucesso';
-    //     }
-    //     //AQUI É O MÉTODO DE EDIÇÃO, VERIFICAMOS SE O ID É DIFERENTE DE NULO E O INPUT É DIFERENTE DE VAZIO
-    //     if ($req->input('_token') != '' && $req->id != '') {
-    //         $fornecedor = Fornecedor::find($req->id); //BUSCANDO PELO ID
-    //         $update = $fornecedor->update($req->all());
-    //         //Aqui é a saida se deu certo o registro é alterado com sucesso, se não o registro não é alterado.
-    //         if($update){ //Se ocorre o update, faz as mensagens de acordo para a views.
-    //             $msg = "Update realizado com sucesso";
-    //         }else{
-    //             $msg = "Update apresentou problemas";
-    //         }
-    //         return redirect()->route('app.fornecedor.editar', ['id'=> $fornecedor->id, 'msg' => $msg, ]);
-    //     }
-    //     // print_r($req->all()); //Mostrando os dados do formulario com o _token
-    //     //OBS: NESSE RETORNO FOI UMA MODIFICAÇÃO FEITA POR EU, PARA PASSA A VARIAVEL FORNECEDOR PARA LA, PÓS EDIÇÃO EU QUERO QUE OS DADOS PERSITEM
-    //     return view('app.fornecedor.adicionar', ['msg' => $msg]); //Temos um problema que pode causar sobrecarga de dados, ao atualiza a pagina toda hora o request é mandado
-    //     //Aqui ocorre uma sobrecarga do banco de dados se você redirecionar para a view, ou seja. Temos um problema aqui.
-    // }
     public function editar($id, $msg = ''){ //Definindo um valor default para msg
         $fornecedor = Fornecedor::find($id); //Recuperando os dados do Fornecedor pelo $id, no qual vai ser selecionado na listagem. 
         // Aqui estamos reutilizando uma view, isso é muito importante e deveras interessante
